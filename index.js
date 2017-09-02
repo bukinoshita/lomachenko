5d195e322c54affe3595a24bd19174591a9c4a93
Bu Kinoshita <bukinoshita@gmail.com>
2017-09-02 01:44:14 -0400

    lomachenko

---
+++ index.js
@@ -0,0 +1,44 @@
+#!/usr/bin/env node
+'use strict'
+
+const arrayShuffle = require('array-shuffle')
+const meow = require('meow')
+const updateNotifier = require('update-notifier')
+const Table = require('cli-table2')
+
+const cli = meow(
+  `
+  Usage:
+    $ lomachenko
+
+  Options:
+    -h, --help                    show help options
+    -v, --version                 show version
+`,
+  {
+    alias: {
+      h: 'help',
+      v: 'version'
+    }
+  }
+)
+
+updateNotifier({ pkg: cli.pkg }).notify()
+
+const run = () => {
+  const arr = length => [...new Array(length)].map((m, i) => i + 1)
+  const shuffled = arrayShuffle(arr(25))
+  const table = new Table()
+
+  table.push(
+    shuffled.slice(0, 5),
+    shuffled.slice(5, 10),
+    shuffled.slice(10, 15),
+    shuffled.slice(15, 20),
+    shuffled.slice(20, 25)
+  )
+
+  console.log(table.toString())
+}
+
+run()
